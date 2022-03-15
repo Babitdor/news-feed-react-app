16f750f2c3bff45bd7d0c9667f657edc6dacc012
Babitdor Kayang Khonglah <57758789+Babitdor@users.noreply.github.com>
2022-03-15 13:02:31 +0530

    Add files via upload

--- components/Bottomnav.js
+++ components/Bottomnav.js
@@ -13,31 +13,31 @@ const Tab = createBottomTabNavigator();
 export default function BottomNav() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator screenOptions={{tabBarStyle:{position:'absolute'}}}>
         <Tab.Screen name="All" component={All} options={{
             tabBarIcon: (props) => (
-              <Icon type='feather' name='home' color={props.color} />
-            ),
+              <Icon type='ionicon' name='home' color={props.color} />
+            ),tabBarLabel:() =>{return null},
           }} />
         <Tab.Screen name="Business" component={BusinessComp} options={{
             tabBarIcon: (props) => (
               <Icon type='feather' name='dollar-sign' color={props.color} />
-            ),
+            ),tabBarLabel:() =>{return null},
           }}/>
         <Tab.Screen name="Health" component={HealthComp} options={{
             tabBarIcon: (props) => (
-              <Icon type='feather' name='heart' color={props.color} />
-            ),
+              <Icon type='ionicon' name='heart-sharp' color={props.color} />
+            ),tabBarLabel:() =>{return null},
           }}/>
         <Tab.Screen name="Sports" component={SportsComp} options={{
             tabBarIcon: (props) => (
               <Icon type='ionicon' name='football' color={props.color} />
-            ),
+            ),tabBarLabel:() =>{return null},
           }}/>
         <Tab.Screen name="Technology" component={TechComp} options={{
             tabBarIcon: (props) => (
-              <Icon type='ionicon' name='hardware-chip-outline' color={props.color} />
-            ),
+              <Icon type='ionicon' name='hardware-chip-sharp' color={props.color} />
+            ),tabBarLabel:() =>{return null},
           }}/>
       </Tab.Navigator>
     </NavigationContainer>
